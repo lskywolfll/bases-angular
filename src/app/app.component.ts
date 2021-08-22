afb9d14004954674230e682244744b56b689d148
Rene <renealejandrosanchezmorales@gmail.com>
2021-08-22 17:31:46 -0400

    created a module contador and heroes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,15 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title: string = 'Counter App';
-  counter: number = 0;
-  base: number = 5;
-
-  incrementCounter() {
-    this.counter += this.base;
-  }
-
-  decrementCounter() {
-    this.counter -= this.base;
-  }
+  
 }
