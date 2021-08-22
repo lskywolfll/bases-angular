08903c97d7bd990752830c952904ab187434c1c0
Rene <renealejandrosanchezmorales@gmail.com>
2021-08-21 20:10:23 -0400

    created counter app sample

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Counter App';
+  counter: number = 0;
+
+  incrementCounter(){
+    this.counter++
+  }
+
+  decrementCounter(){
+    this.counter--
+  }
 }
