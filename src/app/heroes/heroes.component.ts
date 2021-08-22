c3b2b9ece2483d906cfc0b4ce2fca075eef7900c
Rene <renealejandrosanchezmorales@gmail.com>
2021-08-22 17:53:28 -0400

    added changes to html and functions to use inside

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -3,5 +3,25 @@ import { Component } from "@angular/core";
 @Component({
     selector: 'app-heroes',
     templateUrl: 'heroes.component.html',
+    styleUrls: ['../app.component.css'],
 })
-export class HeroesComponent{}
+export class HeroesComponent{
+    nombre: string = "Ironman";
+    edad: number = 45;
+    
+    get nombreCapitalizado() : string {
+        return this.nombre.toUpperCase();
+    }
+    
+    obtenerNombre(): string {
+        return `${this.nombre} - ${this.edad}`
+    }
+
+    cambiarNombre(): void {
+        this.nombre = "Spiderman";
+    }
+
+    cambiarEdad() : void {
+        this.edad -= 10
+    }
+}
