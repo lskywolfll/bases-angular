f56ef92f82d278252fae4f634bfb7e990470f508
Rene <renealejandrosanchezmorales@gmail.com>
2021-09-01 20:16:56 -0400

    added ngFor

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { ContadorComponent } from './contador/contador.component';
 import { HeroesComponent } from './heroes/heroes.component';
+import { ListadoComponent } from './heroes/listado/listado.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContadorComponent,
     HeroesComponent,
+    ListadoComponent,
   ],
   imports: [
     BrowserModule
