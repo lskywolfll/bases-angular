cceb8daa8ec873997220edf57e6102a9e2c83af6
Rene <renealejandrosanchezmorales@gmail.com>
2021-09-09 20:58:04 -0300

    added files to structure append new character on componenet

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -3,13 +3,15 @@ import { FormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { MainPageComponent } from './main-page/main-page.component';
 import { PersonajesComponent } from './personajes/personajes.component';
+import { AgregarComponent } from './agregar/agregar.component';
 
 
 
 @NgModule({
   declarations: [
     MainPageComponent,
-    PersonajesComponent
+    PersonajesComponent,
+    AgregarComponent
   ],
   imports: [
     CommonModule,
--- src/app/dbz/main-page/main-page.component.ts
+++ src/app/dbz/main-page/main-page.component.ts
@@ -6,34 +6,23 @@ import { Personaje } from '../interfaces/dbz.interface';
   templateUrl: './main-page.component.html',
 })
 export class MainPageComponent {
-
   personajes: Personaje[] = [
     {
       nombre: 'Goku',
-      poder: 15000
+      poder: 15000,
     },
     {
       nombre: 'Vegeta',
-      poder: 7500
-    }
+      poder: 7500,
+    },
   ];
 
   nuevo: Personaje = {
     nombre: '',
     poder: 0,
+  };
 
-  }
-
-  agregar(): void {
-
-    if (this.nuevo.nombre.trim().length == 0){
-      return;
-    }
-
-    this.personajes.push(this.nuevo)
-    this.nuevo = {
-      nombre: '',
-      poder: 0,
-    }
+  agregarNuevoPersonaje(character: Personaje) {
+    this.personajes.push(character);
   }
 }
